713bec945997a2d8141aef07c78087912b4b7aaa
wendikardian <wendikardian@gmail.com>
2022-05-21 23:11:34 +0700

    thanks

--- Pertemuan 13/Praktikum Binary Tree/case1.c
+++ Pertemuan 13/Praktikum Binary Tree/case1.c
@@ -1,5 +1,6 @@
 // Wendi Kardian (2100016)
-// Binary Tree
+// Binary Tree 
+// Case 1 - Silsilah Keluarga 
 
 
 #include<stdio.h>
@@ -7,16 +8,20 @@
 #include<string.h>
 #include<malloc.h>
 
+// ADT untuk binary tree
 typedef struct vertex{
     char label[20];
     struct vertex* right;
     struct vertex* left;
 }Vertex;
 
+
+// ADT untuk tree yang berisikan root atau akar dari tree itu
 typedef struct tree{
     Vertex *root;
 }Tree;
 
+// Menginisialisasikan tree baru untuk root yang nanti akan berisikan label/data
 void createTree(char a[], Tree *T){
     Vertex *new;
     new = (Vertex *)malloc(sizeof(Vertex));
@@ -26,30 +31,33 @@ void createTree(char a[], Tree *T){
     (*T).root = new;
 }
 
+// Menambahkan daun/vertex yang berada disebelah kanan
 void addRight(char c[], Vertex *root){
-    if(root->right == NULL){
+    if(root->right == NULL){ //apabila sebelah kanan masih kosong
         Vertex *new = (Vertex *)malloc(sizeof(Vertex));
         strcpy(new->label, c);
         new->right = NULL;
         new->left = NULL;
         root->right = new;
-    }else{
+    }else{ //apabila sebelah kanan sudah di isi
         printf("The right side of the tree already contain\n");
     }
 }
 
+// Menambahkan daun/vertex yang berada disebelah kiri
 void addLeft(char c[], Vertex *root){
-    if(root->left == NULL){
+    if(root->left == NULL){ //apabila sebelah kiri masih kosong
         Vertex *new = (Vertex *)malloc(sizeof(Vertex));
         strcpy(new->label, c);
         new->right = NULL;
         new->left = NULL;
         root->left = new;
-    }else{
+    }else{ //apabila sebelah kiri sudah terpenuhi
         printf("The left side of the tree already contain\n");
     }
 }
 
+// Menghapus semua child yang berada dalam tree tersebut
 void delAll(Vertex *root){
     if(root != NULL){
         delAll(root->left);
@@ -58,6 +66,7 @@ void delAll(Vertex *root){
     }
 }
 
+// Menghapus child yang berada disebelah kanan dari root atau akar
 void delRight(Vertex *root){
     if(root != NULL){
         if(root->right != NULL){
@@ -67,6 +76,7 @@ void delRight(Vertex *root){
     }
 }
 
+// Menghapus child yang berada disebelah kiri dari root atau akar
 void delLeft(Vertex *root){
     if(root != NULL){
         if(root->left != NULL){
@@ -76,7 +86,7 @@ void delLeft(Vertex *root){
     }
 }
 
-
+// Mencetak data secara preorder dengan urutan root-kiri-kanan
 void preOrder(Vertex *root){
     if(root != NULL){
         printf("%s ", root->label);
@@ -85,7 +95,7 @@ void preOrder(Vertex *root){
     }
 }
 
-
+// Mencetak data secara inorder dengan urutan kiri-root-kanan
 void inOrder(Vertex *root){
     if(root != NULL){
         inOrder(root->left);
@@ -94,6 +104,7 @@ void inOrder(Vertex *root){
     }
 }
 
+// Mencetak data secara postorder dengan urutan kiri-kanan-root
 void postOrder(Vertex *root){
     if(root != NULL){
         postOrder(root->left);
@@ -102,10 +113,11 @@ void postOrder(Vertex *root){
     }
 }
 
+// Mengecek apakah isi kedua tree itu sama
 int isEqual(Vertex *root1, Vertex *root2){
     int result = 1;
     if((root1 != NULL) && (root2 != NULL)) {
-        if(root1->label != root2->label){
+        if(strcmp(root1->label, root2->label) != 0){
             result = 0;
         }else{
             isEqual(root1->left, root2->left);
@@ -129,10 +141,22 @@ int main(){
     Vertex* firstRight = T.root->right;
     addLeft("MarkLee", firstLeft);
     addRight("Irene", firstLeft);
+    addLeft("RenJun", firstRight);
+    addRight("Yeri", firstRight);
+    Vertex* Irene = firstLeft->right;
+    addLeft("Chenle", Irene);
+    Vertex* Renjun = firstRight->left;
+    addLeft("Haechan", Renjun);
+    addRight("Jeno", Renjun);
+    Vertex* Jeno = Renjun->right;
+    addLeft("Joy", Jeno);
+    printf("\n====Data PreOrder=====\n");
     preOrder(T.root);
-    printf("\n");
+    printf("\n\n====Data PostOrder=====\n");
     postOrder(T.root);
-    printf("\n");
+    printf("\n\n====Data InOrder=====\n");
     inOrder(T.root);
     printf("\n");
+
+    return 0;
 }
