bcb0d7df8fdda44115f19862f4985ebe35f4535b
wendikardian <wendikardian@gmail.com>
2022-05-21 22:48:17 +0700

    hello

--- Pertemuan 13/Praktikum Binary Tree/case1.c
+++ Pertemuan 13/Praktikum Binary Tree/case1.c
@@ -4,10 +4,11 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<malloc.h>
 
 typedef struct vertex{
-    char label;
+    char label[20];
     struct vertex* right;
     struct vertex* left;
 }Vertex;
@@ -16,19 +17,19 @@ typedef struct tree{
     Vertex *root;
 }Tree;
 
-void createTree(char a, Tree *T){
+void createTree(char a[], Tree *T){
     Vertex *new;
     new = (Vertex *)malloc(sizeof(Vertex));
-    new->label = a;
+    strcpy(new->label, a);
     new->right = NULL;
     new->left = NULL;
     (*T).root = new;
 }
 
-void addRight(char c, Vertex *root){
+void addRight(char c[], Vertex *root){
     if(root->right == NULL){
         Vertex *new = (Vertex *)malloc(sizeof(Vertex));
-        new->label = c;
+        strcpy(new->label, c);
         new->right = NULL;
         new->left = NULL;
         root->right = new;
@@ -37,10 +38,10 @@ void addRight(char c, Vertex *root){
     }
 }
 
-void addLeft(char c, Vertex *root){
+void addLeft(char c[], Vertex *root){
     if(root->left == NULL){
         Vertex *new = (Vertex *)malloc(sizeof(Vertex));
-        new->label = c;
+        strcpy(new->label, c);
         new->right = NULL;
         new->left = NULL;
         root->left = new;
@@ -78,7 +79,7 @@ void delLeft(Vertex *root){
 
 void preOrder(Vertex *root){
     if(root != NULL){
-        printf("%c ", root->label);
+        printf("%s ", root->label);
         preOrder(root->left);
         preOrder(root->right);
     }
@@ -88,7 +89,7 @@ void preOrder(Vertex *root){
 void inOrder(Vertex *root){
     if(root != NULL){
         inOrder(root->left);
-        printf("%c ", root->label);
+        printf("%s ", root->label);
         inOrder(root->right);
     }
 }
@@ -97,22 +98,9 @@ void postOrder(Vertex *root){
     if(root != NULL){
         postOrder(root->left);
         postOrder(root->right);
-        printf("%c ", root->label);
+        printf("%s ", root->label);
     }
 }
-
-// void copyTree(Vertex *root1, Vertex **root2){
-//     if(root1 != NULL){
-//         (*root2) = (Vertex *)malloc(sizeof(Vertex));
-//         (*root2)->label = root1->label;
-//         if(root1->left != NULL){
-//             copyTree(root1->left, (*root2)->left);
-//         }
-//         if(root1->right != NULL){
-//             copyTree(root1->right, (*root2)->right);
-//         }
-//     }
-// }
 
 int isEqual(Vertex *root1, Vertex *root2){
     int result = 1;
@@ -134,12 +122,13 @@ int isEqual(Vertex *root1, Vertex *root2){
 
 int main(){
     Tree T;
-    createTree('A', &T);
-    addLeft('B', T.root);
-    addRight('C', T.root);
-    addLeft('D', T.root->left);
-    addRight('E', T.root->left);
-    addRight('F', T.root->right);
+    createTree("Jaemin", &T);
+    addLeft("Jisung", T.root);
+    addRight("SeulGi", T.root);
+    Vertex* firstLeft = T.root->left;
+    Vertex* firstRight = T.root->right;
+    addLeft("MarkLee", firstLeft);
+    addRight("Irene", firstLeft);
     preOrder(T.root);
     printf("\n");
     postOrder(T.root);
